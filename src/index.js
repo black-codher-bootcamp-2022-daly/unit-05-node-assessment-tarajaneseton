1c865c3f99341f685646ccb255127715e291a1c8
tarajaneseton <tarajaneseton@gmail.com>
2023-02-23 22:17:45 +0000

    todos working

--- src/index.js
+++ src/index.js
@@ -12,30 +12,50 @@ app.use(express.json());
 app.use(express.raw());
 app.use(bodyParser.json());
 
-app.use("/content", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (_, res) => {
-  
- 
+app.get("/", (_req, res) => { 
   res.sendFile("./public/index.html", { root: __dirname }, (err) =>{
     console.log(err);
 });
   // res.end();
 });
 
+//Add GET request with path '/todos to return todos from todos.json file
+
 app.get('/todos', (_, res) => {
-  /*
+  // res.statusCode = 200
   res.header("Content-Type","application/json");
   res.sendFile(todoFilePath, { root: __dirname });
-  */
-  res.status(501).end();
+  res.status(200);
+  // res.status(501).end();
 });
 
-//Add GET request with path '/todos/overdue'
+//Add GET request with path '/todos/overdue'. 
+// Return a list of overdue todos, else if no overdue todos an empty list (array). Todos can be filtered based on due date
+
+// const currentDate = new Date();
+
+// app.get('todos/overdue', function(req, res, next){
+//   if (req.params.date < currentDate) {
+//     res.send
+//   }
+// })
+// app.get('todos/overdue', (req, res) => {
+//   const date = new Date
+//   res.header("Content-Type","application/json");
+//   res.sendFile(todoFilePath, { root: __dirname });
+// } )
 
 //Add GET request with path '/todos/completed'
 
 //Add POST request with path '/todos'
+app.post('/todos', function(req, res) {
+  var newTask = req.body.newtask;
+  //add the new task from the post route
+  task.push(newTask);
+  res.redirect("/");
+});
 
 //Add PATCH request with path '/todos/:id
 
